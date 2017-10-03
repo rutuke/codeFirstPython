d9d8912d07fa742221391450a501761069d6c556
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-03 19:25:28 +0100

    week one

---
+++ hello.py
@@ -0,0 +1,27 @@
+print("Hello World!");
+print(type(2.0));
+
+print(8*9);
+print(6/2);
+print(5.0/2);
+print("Bob" * 3);
+print("hello".upper());
+print("the lord of the rings".title());
+
+age = 5;
+print(age);
+
+like = "painting";
+print(like);
+
+age_description = "My age is {0} and I like {1}.".format(age,like);
+print(age_description);
+
+# Homework
+
+print(10/3);
+print(10.0/3.0);
+print(10%3);
+print(10.0%3.0);
+
+
---
+++ homework.py
@@ -0,0 +1,17 @@
+a = 1;
+a = a + 1;
+print(a); # prints 2
+b = "hello";
+print(b); # prints "hello"
+c = b.title();
+print(b); # prints "hello"
+print(c); # prints "Hello"
+d = "hello";
+e = d.title();
+print(d); # prints "hello"
+print(e); # prints "Hello"
+name = "Dave";
+f = "Hello {0}!".format(name);
+print(f); #prints "Hello Dave!"
+name = "Sarah";
+print(f*5); #prints "Hello Dave!Hello Dave!Hello Dave!Hello Dave!Hello Dave!"
