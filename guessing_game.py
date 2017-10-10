8fd576594ad4d0631594b4d28996cba729b4fe64
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-10 19:55:52 +0100

    week two

---
+++ guessing_game.py
@@ -0,0 +1,19 @@
+import random
+random_number = random.randint(0,10)
+
+def guessed_number():
+    number = input("Guess a number: ")
+    number = int(number)
+    return number
+
+for guess in range(0,5):
+    returned_number = guessed_number()
+    if returned_number > random_number:
+        print("Too high!")
+
+    elif returned_number < random_number:
+        print("Too low!")
+
+    else:
+        print("You guessed right!")
+
--- numbers.py
+++ numbers.py
@@ -8,5 +8,5 @@ if number > 10:
 if number < 0:
     print("Too low! Pick a number between 0 and 10")
 
-else:
-    print("Yay! You picked the right number!")
+#else:
+    #print("Yay! You picked the right number!")
